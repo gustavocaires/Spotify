42daeb56e7cecb41ebd67be82f2d4e1258d71360
Gustavo Caires <gustavocaires@hotmail.com.br>
2024-09-16 21:07:28 -0300

    lista de artistas e album

---
+++ script.js
@@ -0,0 +1,17 @@
+const artistsData = [
+    {name: 'Henrique & Juliano', image: './img/artista-henrique-juliano.jpg'},
+    {name: 'Jorge & Mateus', image: './img/artista-jorge-mateus.jpg'},
+    {name: 'Zé Neto & Cristiano', image: './img/artista-ze-neto.jpg'},
+    {name: 'Gusttavo Lima', image: './img/artista-gustavo-lima.jpg'},
+    {name: 'Luan Santana', image: './img/artista-luan-santana.jpg'},
+    {name: 'Matheus & Kauan', image: './img/artista-mateus-kauan.jpg'},
+]
+
+const albumsData = [
+    {name: 'White Noise (Sleep & Relaxation Sounds)', artists: 'Sleepy John', image: './img/album-white-noise.jpg'},
+    {name: 'O Céu Explica Tudo (Ao Vivo)', artists: 'Henrique & Juliano', image: './img/album-ceu-explica.jpg'},
+    {name: 'Nada Como um Dia Após o Outro', artists: 'Racionais', image: './img/album-vida-loka.jpg'},
+    {name: 'HIT ME HARD AND SOFT', artists: 'Billie Eilish', image: './img/album-hit-me.jpg'},
+    {name: 'CAJU', artists: 'Liniker', image: './img/album-caju.jpg'},
+    {name: 'Escândalo Íntimo', artists: 'Luíza Sonza', image: './img/album-escandalo.jpg'},
+]
